38391e6e9a9744eda3d772018bac9d4f2b846e69
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-10-03 16:27:40 +0200

    Added some html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "./index.css";
+import profileIcon from "/profile-icon.svg";
+import headerIcon from "/title-header.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="head-row">
+        <div className="head-icon">
+          <img src={headerIcon} />
+        </div>
+        <p3>Cohort Manager</p3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="main-body">
+        <div className="left-menu">
+          <h3>Profile</h3>
+          <h3>Menu</h3>
+        </div>
+
+        <div className="content-feed">
+          <div className="make-content-form">
+            <div className="make-content-pr-img">
+              <img src={profileIcon} alt="" />
+            </div>
+
+            <input
+              type="text"
+              name="post-comment"
+              placeholder="What's on your mind?"
+            />
+
+            <button className="post-button">Post</button>
+          </div>
+
+          <div className="post-container">
+            <div className="post-overview">
+              <div className="post-profile-img">
+                <img src={profileIcon} />
+              </div>
+
+              <div className="name-title-post">
+                <div className="name-post">
+                  <p>Sam Fletcher</p>
+                </div>
+
+                <div className="title-post">
+                  <p>Ea sport is not a real game manufacturer</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="full-title-post">
+              <p>Losadkasdkda</p>
+            </div>
+
+            <hr width="100%" size="2"/>
+
+            <div className="add-comment-container">
+              <div className="make-content-form">
+                <div className="make-content-pr-img">
+                  <img src={profileIcon} alt="" />
+                </div>
+
+                <input
+                  type="text"
+                  name="add-comment"
+                  placeholder="Add a comment..."
+                />
+                <button className="send-button">Send</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
